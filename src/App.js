1ba4f3dffb654d55749bfecb35469c2aaad0b665
Deodeep kunj <77825051+deodeepkunj@users.noreply.github.com>
2025-07-31 23:03:18 +0530

    Code added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Deploy react app using <code>GitHub Actions</code> and {""}
+          <code>AWS EC2</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
